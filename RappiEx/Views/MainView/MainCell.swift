14a52a7c80ec0fabb7b8328e51bb2897377eaf38
Pablo <lisadz2@hotmail.com>
2019-06-13 18:58:35 -0500

    Main cell
    se agregó la celda del collectionview de la interfaz inicial

---
+++ RappiEx/Views/MainView/MainCell.swift
@@ -0,0 +1,14 @@
+//
+//  MainCell.swift
+//  RappiEx
+//
+//  Created by Pablo Ramirez on 6/13/19.
+//  Copyright © 2019 Pablo Ramirez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainCell: UICollectionViewCell{
+    
+}
